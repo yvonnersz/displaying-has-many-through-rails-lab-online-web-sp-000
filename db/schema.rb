41b0fa1252739021528f06ef004ac50f1d3a7b89
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-19 10:54:19 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200619082024) do
+ActiveRecord::Schema.define(version: 20200619105309) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
